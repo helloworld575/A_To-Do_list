d5f4ef13e7ec544331d7b9ab981127cd603bf195
zhichenli6 <1471585088@qq.com>
2016-07-12 17:47:21 +0800

    Model for list Items and associated migration

--- lists/tests.py
+++ lists/tests.py
@@ -4,6 +4,7 @@ from django.http import HttpRequest
 from django.template.loader import render_to_string
 from django.views import *
 from lists.views import home_page
+from lists.models import Item
 
 class HomePageTest(TestCase):
 
@@ -15,7 +16,7 @@ class HomePageTest(TestCase):
         request = HttpRequest()
         response=home_page(request)
         expected_html=render_to_string('home.html')
-        print(response.content.decode().strip()+'\n'+expected_html)
+        print(response.content.decode()+'\n'+expected_html)
         self.assertEqual(response.content.decode(),expected_html)
 
     def test_home_page_can_save_a_POST_request(self):
@@ -25,3 +26,21 @@ class HomePageTest(TestCase):
 
         response=home_page(request)
         self.assertIn('A new list item',response.content.decode())
+
+class ItemModelTest(TestCase):
+    def test_saving_and_retrieving_items(self):
+        first_item=Item()
+        first_item.text='The first(ever) list item'
+        first_item.save()
+
+        second_item=Item()
+        second_item.text='Item the second'
+        second_item.save()
+
+        saved_items=Item.objects.all()
+        self.assertEqual(len(saved_items),2)
+
+        first_saved_item=saved_items[0]
+        second_saved_item=saved_items[1]
+        self.assertEqual(first_saved_item.text,'The first(ever) list item')
+        self.assertEqual(second_saved_item.text,'Item the second')
