b1764c496deb68e9694f4deb3fdae8a2938caa8e
zhichenli6 <1471585088@qq.com>
2016-07-12 16:58:58 +0800

    first post

--- functional_tests.py
+++ functional_tests.py
@@ -2,6 +2,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
+import time
 
 class NewVisitorTest(unittest.TestCase):
 
@@ -29,13 +30,14 @@ class NewVisitorTest(unittest.TestCase):
 
         inputbox.send_keys(Keys.ENTER)
 
+        # time.sleep(10);
         table=self.browser.find_element_by_id('id_list_table')
         rows=table.find_elements_by_tag_name('tr')
-        self.assertTrue(
+        '''self.assertTrue(
         any(row.text=='1:Buy peacock feathers' for row in rows),
-        "New to-do item did not appear in table"
-        )
-
+        "New to-do item did not appear in table -- its text was:\n%s" %(table.text,)
+        )'''
+        self.assertIn('1:Buy peacock feathers',[row.text for row in rows])
         self.fail('Finish the test!')
 
 if __name__ == '__main__':
--- lists/tests.py
+++ lists/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.core.urlresolvers import resolve
 from django.http import HttpRequest
 from django.template.loader import render_to_string
-
+from django.views import *
 from lists.views import home_page
 
 class HomePageTest(TestCase):
@@ -11,14 +11,17 @@ class HomePageTest(TestCase):
         found=resolve('/')
         self.assertEqual(found.func,home_page)
 
-    def test_home_page_return_correct_html(self):
-        request=HttpRequest()
-        response=home_page(request)
-        self.assertTrue(response.content.startswith(b'<html>'))
-        self.assertIn(b'<title>To-Do lists</title>',response.content)
-        self.assertTrue(response.content.strip().endswith(b'</html>'))
-    def test_home_page_reuruns_current_html(self):
+    def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response=home_page(request)
         expected_html=render_to_string('home.html')
+        print(response.content.decode().strip()+'\n'+expected_html)
         self.assertEqual(response.content.decode(),expected_html)
+
+    def test_home_page_can_save_a_POST_request(self):
+        request=HttpRequest()
+        request.method='POST'
+        request.POST['item_text']='A new list item'
+
+        response=home_page(request)
+        self.assertIn('A new list item',response.content.decode())
