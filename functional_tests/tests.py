ada766416aeb9657e0a429f57026238cb9c28367
zhichenli6 <1471585088@qq.com>
2016-08-02 13:54:40 +0800

    have changed the functional_test,to realize every user can start a new url and can't see others' list

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -34,6 +34,8 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys('Buy peacock feathers')
 
         inputbox.send_keys(Keys.ENTER)
+        edith_list_url=self.browser.current_url
+        self.assertRegex(edith_list_url,'/lists/.+')
         self.check_for_row_in_list_table('1:Buy peacock feathers')
 
         inputbox=self.browser.find_element_by_id('id_new_item')
@@ -42,11 +44,23 @@ class NewVisitorTest(LiveServerTestCase):
         self.check_for_row_in_list_table('1:Buy peacock feathers')
         self.check_for_row_in_list_table('2:Use peacock feathers to make a fly')
         # time.sleep(10);
-        table=self.browser.find_element_by_id('id_list_table')
-        rows=table.find_elements_by_tag_name('tr')
-        '''self.assertTrue(
-        any(row.text=='1:Buy peacock feathers' for row in rows),
-        "New to-do item did not appear in table -- its text was:\n%s" %(table.text,)
-        )'''
-        self.assertIn('1:Buy peacock feathers',[row.text for row in rows])
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        self.browser.get(self.live_server_url)
+        page_text=self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers',page_text)
+        self.assertNotIn('make a fly',page_text)
+
+        inputbox=self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('But milk')
+        inputbox.send_keys(Keys.ENTER)
+
+        francis_list_url=self.browser.current_url
+        self.assertRegex(francis_list_url,'/lists/.+')
+        self.assertNotEqual(francis_list_url,edith_list_url)
+
+        page_text=self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers',page_text)
+        self.assertIn('Buy milk',page_text)
         self.fail('Finish the test!')
