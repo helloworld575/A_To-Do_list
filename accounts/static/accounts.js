71a9f72880866730a54d834969b792e53aed3922
helloworld575 <zhichenli6@gmail.com>
2016-09-08 20:52:29 +0800

    Add some thing to modify and debug the relog bug

--- accounts/static/accounts.js
+++ accounts/static/accounts.js
@@ -4,6 +4,9 @@ var initialize = function(navigator, user, token, urls) {
     $('#id_login').on('click', function() {
         navigator.id.request();
     });
+    $('#id_logout').on('click', function() {
+        navigator.id.logout();
+    });
 
     navigator.id.watch({
         loggedInUser: user,
@@ -19,7 +22,18 @@ var initialize = function(navigator, user, token, urls) {
                     navigator.id.logout();
                 });
         },
-        onlogout: function() {}
+        onlogout: function() {
+            $.post(
+                urls.logout,
+                {csrfmiddlewaretoken: token}
+            )
+                .done(function() {
+                    window.location.reload();
+                })
+                .fail(function() {
+                    navigator.id.logout();
+                });
+        }
     });
 };
 
